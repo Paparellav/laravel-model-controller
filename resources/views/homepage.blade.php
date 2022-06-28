f5fed26351cc1e1f8f25f3d518b3e638d6484cf0
Paparellav <vitantoniopaparella@gmail.com>
2022-06-28 13:41:43 +0200

    bonus completato

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,23 +1,42 @@
-<h1>Lista film:</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-<ul>
-    @foreach ($films as $film)
-        <li>
-            <small>
-                {{$film->title}}
-            </small>
-            <small>
-                | {{$film->original_title}}
-            </small>
-            <small>
-                | {{$film->nationality}}
-            </small>
-            <small>
-                | {{$film->date}}
-            </small>
-            <small>
-                | {{$film->vote}}
-            </small>
-        </li>
-    @endforeach
-</ul>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Films | Homepage</title>
+    {{-- Styles --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+    <h1>Lista film:</h1>
+    <div class="container">
+        @foreach ($films as $film)
+            <div class="card">
+                <ul>
+                    <li>
+                        <small>
+                            Titolo: {{ $film->title }}
+                        </small>
+                        <small>
+                            Titolo originale: {{ $film->original_title }}
+                        </small>
+                        <small>
+                            Nazionalità: {{ $film->nationality }}
+                        </small>
+                        <small>
+                            Data uscita: {{ $film->date }}
+                        </small>
+                        <small>
+                            Voto: {{ $film->vote }}
+                        </small>
+                    </li>
+                </ul>
+            </div>
+        @endforeach
+    </div>
+</body>
+
+</html>
